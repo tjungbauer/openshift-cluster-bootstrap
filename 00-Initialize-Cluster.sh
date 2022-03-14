80871564e44d8bb1eebd5c03d25d006c2990648d
Thomas Jungbauer <tjungbau@redhat.com>
2022-03-14 08:27:56 +0100

    .

--- 00-Initialize-Cluster.sh
+++ 00-Initialize-Cluster.sh
@@ -5,8 +5,11 @@ NC='\033[0m' # No Color
 
 
 OPT_DRY_RUN='false'
+SECRET_MGMT=''
 OVERLAY='default'
 KUSTOMIZE="/usr/bin/env kustomize"
+HELM="/usr/bin/env helm"
+
 
 function showhelp() {
     cat <<EOF
@@ -25,6 +28,9 @@ while getopts ':d:o:h' 'OPTKEY'; do
     case ${OPTKEY} in
         'd')
             OPT_DRY_RUN='true'
+            ;;
+        's')
+            SECRET_MGMT=''
             ;;
         'o')
             OVERLAY=${OPTARG}
@@ -60,30 +66,81 @@ function error() {
     exit 1
 }
 
+# Get available overlays for openshift-gitops deployment
+function get_available_overlays() {
+    ls -1 bootstrap/openshift-gitops/overlays/
+}
+
+# Ask which Secret Manager should be installed
+function verify_secret_mgmt() {
+  printf "\nPlease select one of the supported Secrets Manager.\n\n"
+  printf "Press 1, 2, 3\n"
+  select svn in "Sealed-Secrets" "Hashicorp-Vault" "None"; do
+        case $svn in
+            Sealed-Secrets ) echo -e "Installing Sealed Secrets"; install_sealed_secrets; break;;
+            Hashicorp-Vault) echo "Installing Hashicorp Vault"; starting_vault; break;;
+            None) echo "Using NO Secret Manager. Exiting"; exit;; 
+        esac
+    done  
+}
+
+# Deploy openshift-gitops
 function deploy() {
-  $KUSTOMIZE 2>&1 >/dev/null || error "Could not execute kustomize binary!"
-
   printf "\n${RED}Deploy GITOPS${NC}\n"
-  $KUSTOMIZE build bootstrap/openshift-gitops/overlays/"$OVERLAY" | oc apply $DRY_RUN -f -
+  $KUSTOMIZE build bootstrap/openshift-gitops/overlays/"${OVERLAY}" | oc apply $DRY_RUN -f -
 
+  verify_secret_mgmt
+}
+
+# Deploy Sealed Secrets if selected
+function install_sealed_secrets() {
   printf "\n${RED}Deploy Selaed Secrets${NC}\n"
   $KUSTOMIZE build bootstrap/sealed-secrets/base | oc apply $DRY_RUN -f - 
 }
 
-if [ "$OVERLAY" == "default" ]; then
+# Deploy Hashicorp Vault if selected
+function starting_vault() {
 
-    cat<<EOF
-This will bootstrap the GitOps operator with the "$OVERLAY" overlay.
+  $HELM 2>&1 >/dev/null || error "Could not execute helm binary!"
+
+  printf "\nWhile the Helm chart automatically sets up complex resources and exposes the configuration to meet your requirements, 
+it does not automatically operate Vault. ${RED}You are still responsible for learning how to operate, monitor, backup, upgrade, etc. the Vault cluster.${NC}\n"
+
+  printf "\nDo you understand?\n"
+
+  select vault in "Yes" "No"; do
+        case $vault in
+            Yes ) echo -e "Installing Hashicorp Vault. NOTE: The values files to overwrite the settings can be found at bootstrap/vault/overwrite-values.yaml"; install_vault; break;;
+            No) echo "Exiting"; exit;;
+        esac
+  done 
+
+}
+
+function install_vault() {
+
+  printf "\n${RED}Deploy Hashicorp Vault${NC}\n"
+  $HELM repo add hashicorp https://helm.releases.hashicorp.com
+  $HELM repo update
+  $HELM upgrade --install vault hashicorp/vault --values bootstrap/vault/overwrite-values.yaml --namespace=vault --create-namespace
+}
+
+$KUSTOMIZE 2>&1 >/dev/null || error "Could not execute kustomize binary!"
+
+printf "This will bootstrap the GitOps operator with the ${RED}${OVERLAY}${NC} overlay.
 If this is not what you want you can specify a different overlay via
-$0 <overlay name>
-EOF
+$0 -o <overlay name>\n\n"
 
-    echo -e "Currently this repo supports the following overlays:\n"
-    ls -1 bootstrap/openshift-gitops/overlays/
+printf "Currently this repo supports the following overlays:\n\n"
+get_available_overlays
+    
+printf "\nDo you wish to continue and install GitOps using the ${RED}${OVERLAY}${NC} settings?\n\n"
+printf "Press 1 or 2\n"
+select yn in "Yes" "No" "Skip"; do
+    case $yn in
+        Yes ) echo -e "Starting Deployment"; deploy; break;;
+        No ) echo "Exit"; exit;;
+        Skip) echo -e "${RED}Skip deployment of GitOps and continue with Secret Management${NC}"; verify_secret_mgmt; break;;
+    esac
+done
 
-    echo -e "\nHit CTRL+C now to specify a different overlay than '$OVERLAY'"
-    echo -e "Sleeping 10 seconds"
-    sleep 10
-fi
-
-deploy
