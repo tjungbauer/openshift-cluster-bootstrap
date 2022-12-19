caea645b8eb5105a29a124718cba56b4dd7b4e95
Thomas Jungbauer <tjungbau@redhat.com>
2022-12-19 15:00:01 +0100

    added logstack object and S3 secret

--- scripts/aws-s3-controller/create_sealed_secret.sh
+++ scripts/aws-s3-controller/create_sealed_secret.sh
@@ -2,5 +2,5 @@
 #AWS_ACCESS_KEY_ID=
 #AWS_SECRET_ACCESS_KEY=
 
-oc create secret generic ack-s3-user-secrets --dry-run=client --from-env-file=/Users/tjungbau/openshift-aws/secrets.txt -o yaml -n ack-system \
+oc create secret generic ack-s3-user-secrets --dry-run=client --from-env-file=<PATH_TO_YOUR>/secrets.txt -o yaml -n ack-system \
 | kubeseal --controller-namespace=sealed-secrets --controller-name=sealed-secrets --format yaml  > ../../clusters/all/aws-controller-s3/templates/ack-s3-user-secrets.yaml
